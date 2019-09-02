4bb7e3750ec9b7e061804119a1b6365bbdf4224b
niels <niels@zeilemaker.nl>
2019-09-02 13:43:21 +0200

    Did not exit if webserver did not start

--- tests/01_webserver.sh
+++ tests/01_webserver.sh
@@ -16,10 +16,12 @@ WAIT_FOR_HOST="127.0.0.1"
 WAIT_FOR_PORT="8080"
   
 echo Waiting for $WAIT_FOR_HOST to listen on $WAIT_FOR_PORT...
+WEBSERVER_STATUS=1
 for i in `seq 1 30`;
 do
     if nc -z $WAIT_FOR_HOST $WAIT_FOR_PORT; then
         echo "process is listening, done"
+        WEBSERVER_STATUS=0
         break
     else
         echo "still waiting"
@@ -28,3 +30,4 @@ do
 done
 
 kill $AIRFLOW_PID
+exit $WEBSERVER_STATUS
